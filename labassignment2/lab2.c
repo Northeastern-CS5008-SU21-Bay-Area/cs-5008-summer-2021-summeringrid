bc56b2e952620a8a618e1f266c9a790370268602
summeringrid <66650505+summeringrid@users.noreply.github.com>
2021-06-01 17:53:59 -0700

    Update lab2.c
    sync lab2

--- labassignment2/lab2.c
+++ labassignment2/lab2.c
@@ -1,17 +1,20 @@
-//enter your name here
-// enter your email here
+// Xiaoying Liu
+// liu.xiaoying@northeastern.edu
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #define N 8
 
+// 1,2,3,4,5,6,7,8
+
 int queue[N];
-int front = -1;
+int front = -1;				
 int rear = -1;
 int elementcount = 0;
 
-int peek() {
+int peek()
+{
    return queue[front]; // To see the data at the front of the queue//
 }
 bool isempty()
@@ -21,28 +24,55 @@ bool isempty()
 
 bool isfull()
 {
-    
     return elementcount==N;// checking if the queue is full
 }
 
 
 int enqueue(int data)
 {
-//insert your code here
-}
+	front = 0;
+	if (!isfull())
+	{
+	rear++;
+	queue[rear] = data;
+	elementcount++;
+	printf("Enqueued data is: %d\n", queue[rear]);
+	}else if (isfull())
+	{
+		// todo How should deal with a full enqeue?
+		printf("The queue is full.\n");
+	}
 
+	return queue[front];
+
+}
 
 void dequeue()
 {
-    
-    //insert your code here
-    
+	if (!isempty())
+	{
+		printf("Dequeued data is %d\n", queue[front]);
+		front++;
+		rear++;
+		elementcount--;
+		
+	
+	}else if (isempty())
+	{
+		printf("The queue is empty.\n");
+	}
+	// check if front and rear are equal then set front=0, rear=0
+	// ↑ can I ignore this step?
+
 }
+
 void display()
-{
-    
-    
-    //insert your code here
+{ 
+    printf("Queue is: ");
+    for (int i = front; i < front + elementcount; i++)
+    {
+    	printf("%d ", queue[i]);
+    }
 }
 
 int main()
@@ -56,16 +86,20 @@ enqueue(66);
 enqueue(77);
 enqueue(88);
 enqueue(99);
+// enqueue(00);		// test isfull()
 display();
+// printf("\nElement at front: %d\n",peek());
+// printf("Total number of elements in the queue are %d\n", elementcount);
 printf("\n");
 dequeue();
 dequeue();
 dequeue();
 dequeue();
 display();
+
     
-printf("\n Element at front: %d\n",peek());
-printf(" \nTotal number of elements in the queue are %d\n", elementcount);
+printf("\nElement at front: %d\n",peek());
+printf("Total number of elements in the queue are %d\n", elementcount);
 
         
 return 0;
